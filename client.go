88b63de184976884770a09d4fd96b6674e723ccd
fang.liu <i@liufang.org.cn>
2017-01-09 02:33:38 +0800

    客户端添加重连机制, 添加命令行选项可以指定参数操作, 添加链接认证功能

--- client.go
+++ client.go
@@ -9,60 +9,91 @@ import (
 	"io"
 	"net"
 	"github.com/astaxie/beego/logs"
+	"compress/zlib"
+	"crypto/md5"
+	"encoding/hex"
+	"flag"
 )
 
+var auth_password = ""
+var server_host = ""
+var accessFileName = "/var/log/apache2/access_log"
+
 func main() {
+	p := flag.String("p", "fang", "author password")
+	bmd5 := md5.Sum([]byte(*p))
+	auth_password = hex.EncodeToString(bmd5[:])
+	//init access file
+	accessFileName = *flag.String("f", "/var/log/apache2/access_log", "sync file path")
+	server_host = *flag.String("s", "localhost", "server host")
+
+	for {
+		//loop connect server
+		doSync(accessFileName)
+		logs.Info("reconnect to server")
+	}
+}
+
+//进行文件同步操作
+func doSync(fileName string) {
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	done := make(chan bool)
-	accessFileName := "/var/log/apache2/access_log"
-	// Process events
-	go func(fileName string) {
-
-		//打开监听的文件
-		f, _ := os.Open(fileName)
-		defer f.Close()
-		rd := bufio.NewReader(f)
-		//创建网络链接进行日志传递
-		conn, err := net.Dial("tcp", "localhost:9999")
-		if err != nil {
-			logs.Error("can't connet to localhost:9999")
-		}
-		wbuf := bufio.NewWriterSize(conn, 1518)
-		for {
-			select {
-			case ev := <-watcher.Event:
-				if ev.IsModify() {
-					for {
-						line, err := rd.ReadString('\n')
-						if err != nil || io.EOF == err {
-							break
-						}
-						fmt.Print(line)
-						wbuf.WriteString(line)
-					}
-				} else {
-					log.Println("event:", ev.Name)
-				}
-			case err := <-watcher.Error:
-				log.Println("error:", err)
-				continue
-			}
-		}
-	}(accessFileName)
-
-	err = watcher.Watch(accessFileName)
+	err = watcher.Watch(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer watcher.Close()
 
-	// Hang so program doesn't exit
-	<-done
+	//打开监听的文件
+	f, _ := os.Open(fileName)
+	//移动指针到末尾
+	f.Seek(0, os.SEEK_END)
+	defer f.Close()
+	rd := bufio.NewReader(f)
+	//丢弃一行日志, 防止出现半行情况
+	rd.ReadString('\n')
+	//创建网络链接进行日志传递
+	conn, err := net.Dial("tcp", server_host + ":9999")
+	if err != nil {
+		logs.Error("can't connet to "+server_host+":9999")
+	}
 
-	/* ... do stuff ... */
-	watcher.Close()
+	zwr := zlib.NewWriter(conn)
+	//进行认证操作
+	n, err := zwr.Write([]byte(auth_password + "\n"));
+	if n == 0 || err != nil {
+		logs.Error("author error, on write")
+		return
+	}
+	defer zwr.Close()
+	for {
+		select {
+		case ev := <-watcher.Event:
+			if ev.IsModify() {
+				for {
+					line, err := rd.ReadString('\n')
+					if err != nil || io.EOF == err {
+						break
+					}
+					fmt.Print(line)
+					n, err := zwr.Write([]byte(line))
+					if n == 0 || err != nil {
+						//TODO 重连
+						logs.Error("write data to server error")
+						return
+					}
+					zwr.Flush()
+				}
+			} else {
+				log.Println("event:", ev.Name)
+			}
+		case err := <-watcher.Error:
+			log.Println("error:", err)
+			continue
+		}
+	}
 }
 
--- server.go
+++ server.go
@@ -5,9 +5,19 @@ import (
 	"bufio"
 	"github.com/astaxie/beego/logs"
 	"fmt"
+	"compress/zlib"
+	"crypto/md5"
+	"encoding/hex"
+	"flag"
 )
 
+var auth_password = ""
+
 func main() {
+	p := flag.String("p", "fang", "author password")
+	bmd5 := md5.Sum([]byte(*p))
+	auth_password = hex.EncodeToString(bmd5[:])
+
 	ln, err := net.Listen("tcp", ":9999")
 	if err != nil {
 		logs.Error("can't bind port 9999")
@@ -26,13 +36,28 @@ func main() {
 //链接数据处理
 //TODO 考虑多个协程输出干扰
 func handleConnection(conn net.Conn) {
-	rbuf := bufio.NewReader(conn)
+	r, _ := zlib.NewReader(conn)
+	rbuf := bufio.NewReader(r)
+
+	str, _ := rbuf.ReadString('\n')
+	if !author(str) {
+		logs.Error("token error, conn close")
+		conn.Close()
+		return
+	}
+	logs.Info("autho success")
+
 	for {
 		str, err := rbuf.ReadString('\n')
 		if err != nil {
 			logs.Error(err)
 			break
 		}
+
 		fmt.Print(str)
 	}
+}
+
+func author(token string) (bool) {
+	return auth_password + "\n" == token
 }
